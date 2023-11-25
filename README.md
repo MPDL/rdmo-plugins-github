483d40655fa323f2f73478ed123eed0f0205c5f3
Jochen Klar <mail@jochenklar.de>
2023-11-25 11:38:20 +0100

    Add GitHubImport

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 rdmo-github
 ===========
 
-This plugin implements an [issue provider](https://rdmo.readthedocs.io/en/latest/plugins/index.html#issue-providers) for RDMO, which lets users push their tasks from RDMO to GitHub issues. The plugin uses [OAUTH 2.0](https://oauth.net/2/), so that users use their respective accounts in both systems.
+This repo implements two plugins for [RDMO](https://github.com/rdmorganiser/rdmo):
+
+* an [issue provider](https://rdmo.readthedocs.io/en/latest/plugins/index.html#issue-providers), which lets users push their tasks from RDMO to GitHub issues.
+* a [project import plugins](https://rdmo.readthedocs.io/en/latest/plugins/index.html#project-import-plugins), which can be used to import projects from (public or private)repos.
+
+The plugin uses [OAUTH 2.0](https://oauth.net/2/), so that users use their respective accounts in both systems.
 
 
 Setup
@@ -13,15 +18,7 @@ Install the plugin in your RDMO virtual environment using pip (directly from Git
 pip install git+https://github.com/rdmorganiser/rdmo-github
 ```
 
-Add the plugin to `PROJECT_ISSUE_PROVIDERS` in `config/settings/local.py`:
-
-```python
-PROJECT_ISSUE_PROVIDERS += [
-    ('github', _('GitHub Provider'), 'rdmo_github.providers.GitHubProvider'),
-]
-```
-
-In addition, an “App” has to be registered with the particular GitLab instance. For GitLab.com, go to https://github.com/settings/developers and create an application with your RDMO URL as callback URL.
+An *App* has to be registered with GitHub. Go to https://github.com/settings/developers and create an application with your RDMO URL as callback URL.
 
 The `client_id` and the `client_secret` need to be configured in `config/settings/local.py`:
 
@@ -32,10 +29,32 @@ GITHUB_PROVIDER = {
 }
 ```
 
+For the issue provider, add the plugin to `PROJECT_ISSUE_PROVIDERS` in `config/settings/local.py`:
+
+```python
+PROJECT_ISSUE_PROVIDERS += [
+    ('github', _('GitHub Provider'), 'rdmo_github.providers.GitHubProvider'),
+]
+```
+
+For the import, add the plugin to `PROJECT_IMPORTS` in `config/settings/local.py`:
+
+```python
+PROJECT_IMPORTS = [
+    ('github', _('Import from GitHub'), 'rdmo_github.providers.GitHubImport'),
+]
+```
+
 
 Usage
 -----
 
-After the setup, users can add a GitHub intergration to their projects. They need to provide the URL to their repository.  Afterwards, issues can be pushed to the GitHub repo.
+### Issue provider
+
+Users can add a GitHub intergration to their projects. They need to provide the URL to their repository.  Afterwards, issues can be pushed to the GitHub repo.
 
 Additionally, a secret can be added to enable GitHub to communicate to RDMO when an issue has been closed. For this, a webhook has to be added at `<https://github.com/<user>/<repo>/settings/hooks`. The webhook has to point to `https://<rdmo_url>/projects/<project_id>/integrations/<integration_id>/webhook/`, the content type is `application/json` and the secret has to be exactly the secret entered in the integration.
+
+### Project import
+
+Users can import project import files directly from a public or private GitHub repository.
--- rdmo_github/providers.py
+++ rdmo_github/providers.py
@@ -1,10 +1,16 @@
+import base64
 import hmac
 import json
+from urllib.parse import quote
 
+from django import forms
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404, HttpResponse
+from django.shortcuts import redirect, render
 from django.utils.translation import gettext_lazy as _
 
+from rdmo.core.imports import handle_fetched_file
+from rdmo.projects.imports import RDMOXMLImport
 from rdmo.projects.providers import OauthIssueProvider
 
 from .mixins import GitHubProviderMixin
@@ -17,10 +23,7 @@ class GitHubIssueProvider(GitHubProviderMixin, OauthIssueProvider):
                     'The upload of attachments is not supported by GitHub.')
 
     def get_post_url(self, request, issue, integration, subject, message, attachments):
-        repo_url = integration.get_option_value('repo_url')
-        if repo_url:
-            repo = repo_url.replace('https://github.com', '').strip('/')
-            return f'https://api.github.com/repos/{repo}/issues'
+        return integration.get_option_value('repo_url')
 
     def get_post_data(self, request, issue, integration, subject, message, attachments):
         return {
@@ -79,3 +82,53 @@ class GitHubIssueProvider(GitHubProviderMixin, OauthIssueProvider):
                 'secret': True
             }
         ]
+
+
+class GitHubImport(GitHubProviderMixin, RDMOXMLImport):
+
+    class Form(forms.Form):
+        repo = forms.CharField(label=_('GitHub repository'),
+                               help_text=_('Please use the form username/repository or organization/repository.'))
+        path = forms.CharField(label=_('File path'))
+        ref = forms.CharField(label=_('Branch, tag, or commit'), initial='main')
+
+    def render(self):
+        return render(self.request, 'projects/project_import_form.html', {
+            'source_title': 'GitHub',
+            'form': self.Form()
+        }, status=200)
+
+    def submit(self):
+        form = self.Form(self.request.POST)
+
+        if 'cancel' in self.request.POST:
+            if self.project is None:
+                return redirect('projects')
+            else:
+                return redirect('project', self.project.id)
+
+        if form.is_valid():
+            self.request.session['import_source_title'] = self.source_title = form.cleaned_data['path']
+
+            url = '{api_url}/repos/{repo}/contents/{path}?ref={ref}'.format(
+                api_url=self.api_url,
+                repo=quote(form.cleaned_data['repo']),
+                path=quote(form.cleaned_data['path']),
+                ref=quote(form.cleaned_data['ref'])
+            )
+
+            return self.get(self.request, url)
+
+        return render(self.request, 'projects/project_import_form.html', {
+            'source_title': 'GitHub',
+            'form': form
+        }, status=200)
+
+    def get_success(self, request, response):
+        file_content = response.json().get('content')
+        request.session['import_file_name'] = handle_fetched_file(base64.b64decode(file_content))
+
+        if self.current_project:
+            return redirect('project_update_import', self.current_project.id)
+        else:
+            return redirect('project_create_import')
