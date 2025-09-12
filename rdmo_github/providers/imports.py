068ff6e47d3a777ae236a4a4b71efeb9bad83e81
Laura Bahamon <bahamon@mpdl.mpg.de>
2025-09-12 15:52:12 +0200

    update

--- rdmo_github/providers/imports.py
+++ rdmo_github/providers/imports.py
@@ -69,7 +69,7 @@ class GitHubImport(GitHubProviderMixin, RDMOXMLImport):
         else:
             repo = form_data['repo']
 
-        url = self.get_request_url(quote(repo), quote(form_data['path']), quote(form_data['ref'])) 
+        url = self.get_request_url(repo, form_data['path'], form_data['ref'])
         return url
 
     def get_success(self, request, response):
