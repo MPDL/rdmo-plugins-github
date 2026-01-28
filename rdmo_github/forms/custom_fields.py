cd7fb14e6d61d2170fef877579654751803bf3e6
Laura Bahamon <bahamon@mpdl.mpg.de>
2026-01-28 11:32:40 +0100

    MultivalueCheckboxMultipleChoiceField is now sortable, its choices can have a simple checkbox input or a checkbox with a text input, and it can be passed general or choice specific validators and widget attributes

--- rdmo_github/forms/custom_fields.py
+++ rdmo_github/forms/custom_fields.py
@@ -2,30 +2,33 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-from .custom_widgets import CustomChoiceMultiWidget, CustomSelectMultiple
-from .custom_validators import validate_export_file_path
+from .custom_widgets import MultivalueCheckboxWidget, MultivalueCheckboxMultipleChoiceWidget
 
-class CustomChoiceMultiValueField(forms.MultiValueField):
-    widget = CustomChoiceMultiWidget
+class MultivalueCheckboxField(forms.MultiValueField):
+    widget = MultivalueCheckboxWidget
 
-    def __init__(self):
+    def __init__(self, simple_checkbox=False):
         fields = (
-            forms.BooleanField(),
-            forms.CharField(validators=[validate_export_file_path]),
+            (forms.BooleanField(),) if simple_checkbox else
+            (    
+                forms.BooleanField(),
+                forms.CharField(),
+            )
         )
+
         super().__init__(fields)
 
     def clean(self, value):
         """This method applies to a multi-value field corresponding to 
-        a choice in CustomMultipleChoiceField.
+        a choice in MultivalueCheckboxMultipleChoiceField.
         Every export choice consists of a boolean field and a char field.
 
         Validate every subvalue in value ([boolean_value, char_value]). 
         Each subvalue is validated against the corresponding Field in self.fields.
 
         Important: ValidationErrors are NOT raised here, clean() returns
-        the choice's errors to the main field CustomMultipleChoiceField.
-        After validating all choices, CustomMultipleChoiceField 
+        the choice's errors to the main field MultivalueCheckboxMultipleChoiceField.
+        After validating all choices, MultivalueCheckboxMultipleChoiceField 
         raises all ValidationErrors.
         """
 
@@ -40,7 +43,7 @@ class CustomChoiceMultiValueField(forms.MultiValueField):
             field_value = value[i]
 
             if field_value in self.empty_values: # self.empty_values = (None, '', [], (), {})
-                errors.append(ValidationError(_('A file path is required.'), code='required')) 
+                errors.append(ValidationError(_('A file path is required.'), code='required'))
                 
             try:
                 clean_data.append(field.clean(field_value))
@@ -60,33 +63,136 @@ class CustomChoiceMultiValueField(forms.MultiValueField):
             - a string value for the text
         '''
 
-        if isinstance(data_list, list) and len(data_list) == 2:
+        if isinstance(data_list, list):
             return ','.join(map(str, data_list))
         
-        return 'False,'
+        return 'False'
     
 
-class CustomMultipleChoiceField(forms.MultipleChoiceField):
-    # widget = CustomSelectMultiple
-    choice_field = CustomChoiceMultiValueField()
-    _choice_names = []
+class MultivalueCheckboxMultipleChoiceField(forms.MultipleChoiceField):
+    '''
+    A multiple choice field with multivalue checkboxes as choices.
+    
+    CHOICES
+    
+    Each choice consists of one or two fields:
+    - a checkbox field with a label 
+    - (optionally) a text input field with a label
+
+    choices = [(values, labels, key), ...] with:
+
+    1. values (str): comma-separated values for each choice. 
+    First value is required and is the starting value for the checkbox (as a string: 'True' or 'False'), 
+    second value is optional and is the starting value for the text.
+    Examples of valid values:
+    - 'False,my-text' -> checkbox's starting value is False, text's starting value is 'my-text'
+    - 'True,' -> checkbox's starting value is True, text's starting value is ''
+    - 'True' -> checkbox's starting value is True, text does NOT exist (no comma)
+    
+    2. labels (str | tuple[str]): a string or a one or two-value tuple with the checkbox and text field labels (str).
+    First value is required, second is optional.
+    Examples of valid labels:
+    - ('Checkbox label', 'Text label')
+    - ('one value tuple with checkbox label',)
+    - 'string label for checkbox'
+
+    3. key (str): choice key.
+    Examples:
+    - 'choice_1'
+    - 'pdf-export'
+
+    CHOICE VALIDATORS and CHOICE ATTRIBUTES
+
+    Common validators and attributes can be passed as usual (MultivalueCheckboxMultipleChoiceField(..., validators=validators),
+    MultivalueCheckboxMultipleChoiceWidget(..., attrs=attrs)).
+    
+    Choice validators or choice attributes can also be passed as follows:
+
+    choice_validators (dict[choice_key, dict['checkbox'|'text', list[Validators]]]) is a dictionary with choice_keys 
+    as keys and for values dictionaries with validators for the checkbox field and/or the text field. choice_validators 
+    only need to contain choice_keys of choices that need validators, and only the field ('checkbox' or 'text') that
+    needs validators must be included in the inner dictionary. 
+    
+    choice_attributes (dict[choice_key, dict['checkbox'|'text', attrs_dict]]) is a dictionary with choice_keys 
+    as keys and for values dictionaries with attributes for the checkbox widget and/or the text widget. choice_attributes 
+    only need to contain choice_keys of choices with extra attributes, and only the widget ('checkbox' or 'text') that
+    needs extra attributes must be included in the inner dictionary.
+    
+    Examples:
+
+    If there are these choices = ((values_1, labels_1, 'choice_1'), (values_2, labels_2, 'choice_2'), (values_3, labels_3, 'choice_3')),
+    these would be valid choice_validators and choice_attributes:
+    - choice_validators = {
+        'choice_1': {
+            'checkbox': [validator_1, validator_2],
+            'text': [validator_3]
+        },
+        'choice_3: {
+            'text': [validator_3, validator_4, validator_5]
+        }
+    }
+    - choice_attributes = {
+        'choice_1': {
+            'checkbox': {'onchange': changeHandler()}
+        },
+        'choice_2': {
+            'checkbox': {'oninput': inputHandler()},
+            'text': {'style': 'background-color: blue;'}
+        }
+    }
+    '''
+
+    widget = MultivalueCheckboxMultipleChoiceWidget
+    _choice_fields = {}
+    _choice_keys = []
+    _choice_validators = {}
     _choices_to_update = {}
 
-    def __init__(self, *args, **kwargs):
-        sortable = kwargs.pop('sortable', False)
-        super().__init__(*args, **kwargs)
-
-        self.widget = CustomSelectMultiple(
-            sortable=sortable
-        )
+    @property
+    def choices(self):
+        return self._choices
+    
+    @choices.setter
+    def choices(self, new_choices):
+        self._choices = self.widget.choices = new_choices
+        self.choice_fields = new_choices
+        self.choice_keys = new_choices
     
     @property
-    def choice_names(self):
-        return self._choice_names
+    def choice_fields(self):
+        return self._choice_fields
+
+    @choice_fields.setter
+    def choice_fields(self, new_choices):
+        new_choice_fields = {}
+        for c in new_choices:
+            simple_checkbox = False
+            
+            values = c[0].split(',')
+            choice_key = c[2]
+            if isinstance(values, list) and len(values) == 1:
+                simple_checkbox = True
+
+            new_choice_fields[choice_key] = MultivalueCheckboxField(simple_checkbox=simple_checkbox)
+        
+        self._choice_fields = new_choice_fields
+
+    @property
+    def choice_keys(self):
+        return self._choice_keys
     
-    @choice_names.setter
-    def choice_names(self, new_names):
-        self._choice_names = self.widget.choice_names = new_names
+    @choice_keys.setter
+    def choice_keys(self, new_choices):
+        new_choice_keys = [c[2] for c in new_choices]
+        self._choice_keys = self.widget.choice_keys = new_choice_keys
+
+    @property
+    def choice_validators(self):
+        return self._choice_validators
+    
+    @choice_validators.setter
+    def choice_validators(self, new_choices_validators):
+        self._choice_validators = new_choices_validators
 
     @property
     def choices_to_update(self):
@@ -103,13 +209,13 @@ class CustomMultipleChoiceField(forms.MultipleChoiceField):
             raise ValidationError(
                 self.error_messages["invalid_list"], code="invalid_list"
             )
-        
+
         value = [str(multival) for multival in value]
 
-        # value is a list of choices, each choice is a multivalue string with two comma-separated values:
+        # value is a list of choices, each choice is a multivalue string with 1 or two comma-separated values:
         # i = 0 -> choice key (checkbox value, fix)
-        # i = 1 -> file path (input field, modify-able by the user)
-        return [f"{multival.split(',')[0]},{multival.split(',')[1].strip()}" for multival in value]
+        # i = 1 -> optional file path (input field, modify-able by the user)
+        return [','.join(map(lambda x: x.strip(), multival.split(','))) for multival in value]
 
     def clean(self, value):
         '''Validate the given value and return its 'cleaned' value as an
@@ -122,17 +228,40 @@ class CustomMultipleChoiceField(forms.MultipleChoiceField):
         corresponding choice(s). 
         '''
         value = self.to_python(value)
-        # print('clean()')
-        # print(f'    value: {value}')
+
+        if len(self.validators) > 0:
+            self.run_validators()
+
+            return value
+        
+        if len(self.choice_validators) > 0:
+            for choice_key in self.choice_keys:
+                checkbox_validators = self.choice_validators.get(choice_key, {}).get('checkbox')
+                text_validators = self.choice_validators.get(choice_key, {}).get('text')
+                choice_field = self.choice_fields.get(choice_key)
+
+                if checkbox_validators:
+                    choice_field.fields[0].validators = checkbox_validators
+                
+                if text_validators and len(choice_field.fields) > 1:
+                    choice_field.fields[1].validators = text_validators
+                
+
         self.widget.errors = {}
         if value in self.empty_values and self.required: # self.empty_values = (None, '', [], (), {})
             raise ValidationError(_('At least one choice must be selected.'), code='required')
 
         # validate choice values, which consist of multivalues (boolean and char) 
         for multivalue in value:
-            choice_key, text_value = multivalue.split(',')
-            if choice_key in self.choice_names:
-                out, errors = self.choice_field.clean([True, text_value])
+            multivalue_list = multivalue.split(',')
+            choice_key = multivalue_list[0]
+            if len(multivalue_list) > 1:
+                text_value = multivalue_list[1]
+            
+            if choice_key in self.choice_keys:
+                choice_field = self.choice_fields.get(choice_key)
+                choice_value = [True, text_value] if len(multivalue_list) > 1 else [True]
+                out, errors = choice_field.clean(choice_value)
                 if len(errors) > 0:
                     self.widget.errors[choice_key] = errors
                 else:
--- rdmo_github/forms/custom_widgets.py
+++ rdmo_github/forms/custom_widgets.py
@@ -1,16 +1,15 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-class CustomChoiceMultiWidget(forms.MultiWidget):
-    def __init__(self, attrs=None):
+class MultivalueCheckboxWidget(forms.MultiWidget):
+    def __init__(self, simple_checkbox=False, attrs=None):
         widgets = {
-            'checkbox': forms.CheckboxInput(attrs={'onchange': 'toggle_option_attributes_visibility(this)'}),
-            'text': forms.TextInput(attrs={
-                'title': _('Special characters will be percent-encoded'), 
-                'placeholder': _('example_folder/example_file.extension'),
-                'oninput': 'hide_check_message(this)'
-            })
+            'checkbox': forms.CheckboxInput()
         }
+
+        if not simple_checkbox:
+            widgets['text'] = forms.TextInput()
+
         super().__init__(widgets, attrs)
 
     def decompress(self, value):
@@ -18,7 +17,8 @@ class CustomChoiceMultiWidget(forms.MultiWidget):
             - a boolean value for the checkbox
             - a string value for the text
         '''
-         
+        # print('decompress()')
+        # print(f'value: {value}')
         boolean_value = {'False': False, 'True': True}
         if isinstance(value, str):
             splitted_value = value.split(',')
@@ -27,8 +27,8 @@ class CustomChoiceMultiWidget(forms.MultiWidget):
         
         return [False, '']
     
-    def get_context(self, name, value, checkbox_label, checkbox_id, text_id, attrs):
-        '''Create context for CustomSelectMultiple.option_template_name. '''
+    def get_context(self, name, value, checkbox_label, text_label, checkbox_id, text_id, attrs, extra_attrs=None):
+        '''Create context for MultivalueCheckboxMultipleChoiceWidget.option_template_name. '''
         
         context = super().get_context(name, value, attrs)
         # value is a list/tuple of values, each corresponding to a widget
@@ -46,13 +46,18 @@ class CustomChoiceMultiWidget(forms.MultiWidget):
             except IndexError:
                 widget_value = None
             
+            extra_widget_attrs = extra_attrs.get(widget_name.strip('_'), {}) if isinstance(extra_attrs, dict) else {}
+
             widget_attrs = final_attrs.copy()
+            widget_attrs.update(extra_widget_attrs)
             if widget_name == '_text':
-                widget_attrs.update({'id': text_id, 'style': 'flex-grow:1;'})
+                widget_attrs.update({'id': text_id, 'class': 'form-control', 'style': 'flex-grow:1;'})
             if widget_name == '_checkbox':
                 widget_attrs.update({'id': checkbox_id})
             
             widget_context = widget.get_context(name + widget_name, widget_value, widget_attrs)['widget']
+            if widget_name == '_text':
+                widget_context.update({'label': text_label})
             if widget_name == '_checkbox':
                 widget_context.update({'label': checkbox_label})
             subwidgets.append(widget_context)
@@ -61,76 +66,112 @@ class CustomChoiceMultiWidget(forms.MultiWidget):
         return context
     
 
-class CustomSelectMultiple(forms.SelectMultiple):
-    allow_multiple_selected = True
-    add_id_index = True
-    checked_attribute = {'checked': True}
+class MultivalueCheckboxMultipleChoiceWidget(forms.SelectMultiple):
     option_inherits_attrs = True
     errors = {}
-    _choice_names = []
+    _choice_keys = []
+    _choice_widgets = {}
+    _choice_attributes = {}
+    option_template_name = 'plugins/custom_multivalue_select_option.html'
+    template_name = 'plugins/custom_multivalue_select.html'
 
-    choice_widget = CustomChoiceMultiWidget()
-
-    def __init__(self, *args, **kwargs):
-        sortable = kwargs.pop('sortable', False)
-        super().__init__(*args, **kwargs)
+    def __init__(self, *, sortable=False, **kwargs):
+        super().__init__(**kwargs)
 
         self.sortable = sortable
-        self.option_template_name = 'plugins/sortable_custom_multivalue_select_option.html' if sortable else 'plugins/custom_multivalue_select_option.html'
-        self.template_name = 'plugins/sortable_custom_multivalue_select.html' if sortable else 'plugins/custom_multivalue_select.html'
 
     @property
-    def choice_names(self):
-        return self._choice_names
+    def choices(self):
+        return self._choices
     
-    @choice_names.setter
-    def choice_names(self, new_names):
-        self._choice_names = new_names
+    @choices.setter
+    def choices(self, new_choices):
+        self._choices = new_choices
+        self.choice_widgets = new_choices
+
+    @property
+    def choice_widgets(self):
+        return self._choice_widgets
+    
+    @choice_widgets.setter
+    def choice_widgets(self, new_choices):
+        new_choice_widgets = {}
+        for c in new_choices:
+            simple_checkbox = False
+            
+            values = c[0].split(',')
+            choice_key = c[2]
+            if isinstance(values, list) and len(values) == 1:
+                simple_checkbox = True
+
+            new_choice_widgets[choice_key] = MultivalueCheckboxWidget(simple_checkbox=simple_checkbox)
+
+        self._choice_widgets = new_choice_widgets
+    
+    @property
+    def choice_keys(self):
+        return self._choice_keys
+    
+    @choice_keys.setter
+    def choice_keys(self, new_keys):
+        self._choice_keys = new_keys
+
+    @property
+    def choice_attributes(self):
+        return self._choice_attributes
+    
+    @choice_attributes.setter
+    def choice_attributes(self, new_attributes):
+        self._choice_attributes = new_attributes
 
     def sort_choices(self, data, name):
-        selected_choices = [k for k,v in data.items() if (k.startswith(name) and 'on' in v)]
-        sorted_choice_names = [c.removeprefix(f'{name}_').removesuffix('_checkbox') for c in selected_choices]
+        selected_choices = [k for k,v in data.items() if (k.startswith(name) and k.endswith('_checkbox') and 'on' in v)]
+        sorted_choice_keys = [c.removeprefix(f'{name}_').removesuffix('_checkbox') for c in selected_choices]
 
-        for k in self.choice_names:
-            if k not in sorted_choice_names:
-                sorted_choice_names.append(k)
+        for k in self.choice_keys:
+            if k not in sorted_choice_keys:
+                sorted_choice_keys.append(k)
 
         sorted_choices = []
-        for k in sorted_choice_names:
-            choice = next((c for c in self.choices if c[1][1] == k), None)
+        for k in sorted_choice_keys:
+            choice = next((c for c in self.choices if c[2] == k), None)
             if choice is not None:
                 sorted_choices.append(choice)
 
-        return sorted_choice_names, sorted_choices
+        return sorted_choice_keys, sorted_choices
 
     def optgroups(self, name, value, attrs=None):
         '''Return a list of choices for this widget.
         Each choice consists of a multi widget with a checkbox and a text.
         '''
-        print(f'value: {value}')
-        selected_option_keys = [v.split(',')[0] for v in value]
-        print(f'selected_option_keys: {selected_option_keys}')
-        transformed_value = [f'{True},{v.split(",")[-1]}' for v in value]
-        print(f'transformed_value: {transformed_value}')
+        
+        transformed_value = []
+        selected_option_keys = []
+        for v in value:
+            v_list = v.split(',')
+            selected_option_keys.append(v_list[0])
+            transformed_v = f'True,{v_list[1]}' if len(v_list) > 1 else 'True'
+            transformed_value.append(transformed_v)
 
         current_errors = {k: v for k, v in self.errors.items() if k in selected_option_keys}
         self.errors = current_errors
 
         groups = []
-        for index, (option_value, (option_label, option_key)) in enumerate(self.choices):
+        for index, (option_value, option_labels, option_key) in enumerate(self.choices):
             i = selected_option_keys.index(option_key) if option_key in selected_option_keys else None
             option_value = transformed_value[i] if i is not None else option_value
-            decompressed_option_value = self.choice_widget.decompress(option_value)
+            choice_widget = self.choice_widgets[option_key]
+            decompressed_option_value = choice_widget.decompress(option_value)
             
             selected = self.allow_multiple_selected and decompressed_option_value[0]
             
             option_name = f'{name}_{option_key}'
             
             groups.append(self.create_option(
-                self.choice_widget,
+                choice_widget,
                 option_name,
                 option_value,
-                option_label,
+                option_labels,
                 option_key,
                 selected,
                 index,
@@ -140,7 +181,7 @@ class CustomSelectMultiple(forms.SelectMultiple):
         return groups
 
     def create_option(
-        self, widget, name, value, label, key, selected, index, attrs=None
+        self, widget, name, value, labels, key, selected, index, attrs=None
     ):
         '''Create a choice consisting of a multi widget with a checkbox and a text. '''  
         
@@ -148,13 +189,25 @@ class CustomSelectMultiple(forms.SelectMultiple):
         option_attrs = (
             self.build_attrs(self.attrs, attrs) if self.option_inherits_attrs else {}
         )
+
+        checkbox_id = text_id = None
         if 'id' in option_attrs:
-            checkbox_id = self.id_for_label(option_attrs['id'], index)
-            text_id = self.id_for_label(f'{option_attrs["id"]}_text', index)
+            checkbox_id = '%s_%s' % (option_attrs['id'], index)
+            text_id = '%s_%s' % (f'{option_attrs["id"]}_text', index)
             option_attrs = {}
+        
         if selected:
             option_attrs.update(self.checked_attribute)
-        option_context = widget.get_context(name, value, label, checkbox_id, text_id, option_attrs)
+
+        checkbox_label = text_label = ''
+        if isinstance(labels, tuple):
+            checkbox_label = labels[0]
+            text_label = labels[1] if len(labels) > 1 else ''
+        else:
+            checkbox_label = labels
+
+        extra_option_attrs = self.choice_attributes.get(key)
+        option_context = widget.get_context(name, value, checkbox_label, text_label, checkbox_id, text_id, option_attrs, extra_option_attrs)
         choices_to_update = getattr(self, 'choices_to_update', None)
         option_in_repo = self.choices_to_update[key] if choices_to_update and key in self.choices_to_update.keys() else None
         option_errors = self.errors[key] if key in self.errors.keys() else None
@@ -169,30 +222,22 @@ class CustomSelectMultiple(forms.SelectMultiple):
             'template_name': self.option_template_name,
         }
     
-    def id_for_label(self, id_, index='0'):
-        '''Use an incremented id for each option where the main widget
-        references the zero index.
-        '''
-        
-        if id_ and self.add_id_index:
-            id_ = '%s_%s' % (id_, index)
-        return id_
-    
     def value_from_datadict(self, data, files, name):
-        # # print('CustomSelectMultiple.value_from_datadict()')
-        # # print('data')
-        # # print(data)
-        # # print('self.sortable')
-        # # print(f'name: {name}')
-        # # print(f'self.choice_names: {self.choice_names}')
-
         if self.sortable:
-            self.choice_names, self.choices = self.sort_choices(data, name)
+            self.choice_keys, self.choices = self.sort_choices(data, name)
         
         value = []
-        for multiwidget_name in self.choice_names:
-            multiwidget_value = self.choice_widget.value_from_datadict(data, files, f'{name}_{multiwidget_name}')
+        for multiwidget_name in self.choice_keys:
+            choice_widget = self.choice_widgets[multiwidget_name]
+            multiwidget_value = choice_widget.value_from_datadict(data, files, f'{name}_{multiwidget_name}')
+            
             if multiwidget_value[0]:
-                value.append(f'{multiwidget_name},{multiwidget_value[1]}')
+                choice_value = multiwidget_name if len(multiwidget_value) == 1 else f'{multiwidget_name},{multiwidget_value[1]}'
+                value.append(choice_value)
 
         return value
+    
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        context['widget']['sortable'] = self.sortable
+        return context
