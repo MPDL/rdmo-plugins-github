19015e1310dbcefc117a2683392cbe23fc58d33e
Laura Bahamon <bahamon@mpdl.mpg.de>
2026-02-17 11:23:36 +0100

    add make_request method to support put (as well as get and post) requests

--- rdmo_github/mixins.py
+++ rdmo_github/mixins.py
@@ -2,6 +2,7 @@ import logging
 import requests
 from urllib.parse import urlencode, quote
 from requests.auth import HTTPBasicAuth
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 from django.conf import settings
 from django.urls import reverse
@@ -144,6 +145,74 @@ class GitHubProviderMixin(OauthProviderMixin):
         # save values in session
         for k,v in kwargs.items():
             self.store_in_session(request, k, v)
+    
+    def make_request(self, request, method, url, apply_data_processing=False, *args, **kwargs):
+        methods = {
+            'get': {'request_method': requests.get, 'success_method': self.get_success},
+            'post': {'request_method': requests.post, 'success_method': self.post_success},
+            'put': {'request_method': requests.put, 'success_method': self.put_success}
+        }
+        if method not in methods.keys():
+            raise ValueError(f"Unsupported request method: {method}")
+
+        access_token = self.get_from_session(request, 'access_token')
+        if access_token:
+            # if the access_token is available make request to the upstream service
+            logger.debug('%s: %s', method, url)
+
+            request_method, success_method = methods[method].values()
+
+            data_processing_params = {}
+            if 'data_processing_params' in kwargs.keys():
+                data_processing_params = kwargs['data_processing_params']
+            
+            headers = self.get_authorization_headers(access_token)
+            if 'multipart' in kwargs.keys():                
+                multipart = kwargs['multipart']
+                if apply_data_processing:
+                    processed_multipart = self.process_request_data({**multipart}, access_token=access_token, **data_processing_params)
+                    multipart_encoder = MultipartEncoder(fields=processed_multipart)
+                else:
+                    multipart_encoder = MultipartEncoder(fields=multipart)
+                
+                headers['Content-Type'] = multipart_encoder.content_type
+                response = request_method(url, data=multipart_encoder, headers=headers)
+            elif 'files' in kwargs.keys():
+                files = kwargs['files']
+                if apply_data_processing:
+                    processed_files = self.process_request_data({**files}, access_token=access_token, **data_processing_params)
+                    response = request_method(url, files=processed_files, headers=headers)
+                else:
+                    response = request_method(url, files=files, headers=headers)
+            elif 'json' in kwargs.keys():
+                json = kwargs['json']
+                if apply_data_processing:
+                    processed_json = self.process_request_data({**json}, access_token=access_token, **data_processing_params)
+                    response = request_method(url, json=processed_json, headers=headers)
+                else:
+                    response = request_method(url, json=json, headers=headers)
+            else:
+                response = request_method(url, headers=headers)
+
+            if response.status_code == 401:
+                logger.warning('%s forbidden: %s (%s)', method, response.content, response.status_code)
+            else:
+                try:
+                    response.raise_for_status()
+                    return success_method(request, response)
+
+                except requests.HTTPError:
+                    logger.warning('%s error: %s (%s)', method, response.content, response.status_code)
+
+                    return render(request, 'core/error.html', {
+                        'title': _('Request (export or import) error'),
+                        'errors': [_('Something went wrong: %s') % self.get_error_message(response)]
+                    }, status=200)
+
+        # if the above did not work authorize first
+        kwargs['apply_data_processing'] = apply_data_processing
+        self.store_in_session(request, 'request', (method, url, kwargs))
+        return self.authorize(request)
     
     def authorize(self, request):
         installation_id = self.get_from_session(request, 'installation_id')
