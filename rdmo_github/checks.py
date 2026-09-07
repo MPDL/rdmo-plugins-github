c91780abf53768b2fa3c18779a0fbc394a5c335c
Laura Bahamon <laura.bahamon@maxit.mpg.de>
2026-09-07 11:04:46 +0200

    remove github app's key

--- rdmo_github/checks.py
+++ rdmo_github/checks.py
@@ -13,12 +13,12 @@ def check_github_provider_settings(app_configs, **kwargs):
             Error('settings.GITHUB_PROVIDER does not exist.', hint='Add GITHUB_PROVIDER to config/settings/local.py')
         )
     else:
-        for key in ['client_id', 'client_secret', 'app_type', 'github_app_name']:
+        for key in ['client_id', 'client_secret']:
             if not provider.get(key):
-                errors.append(Error(f'Key "{key}" is missing from settings.GITHUB_PROVIDER')
-        )
+                errors.append(Error(f'Key "{key}" is missing from settings.GITHUB_PROVIDER'))
 
     return errors
+
 
 @register()
 def check_settings_installed_apps_includes_rdmo_maus(app_configs, **kwargs):
@@ -27,6 +27,8 @@ def check_settings_installed_apps_includes_rdmo_maus(app_configs, **kwargs):
     installed_apps = settings.INSTALLED_APPS
 
     if 'rdmo_maus' not in installed_apps:
-        errors.append(Error('"rdmo_maus" must be included in settings.INSTALLED_APPS for rdmo_github to properly work.'))
-    
+        errors.append(
+            Error('"rdmo_maus" must be included in settings.INSTALLED_APPS for rdmo_github to properly work.')
+        )
+
     return errors
